9ba92ec8390335386830eb9bbf55c81e1b2ba3b2
kumA0taku <varinpimda@gmail.com>
2022-04-08 00:07:20 +0700

    edit bank screen

--- admin_system/index.php
+++ admin_system/index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include('include/head.php') ?>
-<?php include('include/script.php') ?>
+
 <!-- script -->
+<?php include('include/script.php') ?>
 
 <body class="app">
     <!--//app-header-->
@@ -11,24 +12,29 @@
         <div class="app-content pt-3 p-md-3 p-lg-4">
             <div class="container-xl">
                 <?php
-                if(!isset($_GET['page']) && empty($_GET['page']))
-                {
+                if (!isset($_GET['page']) && empty($_GET['page'])) {
                     include('stock/index.php'); //stock
-                }
-                elseif(isset($_GET['page']) && $_GET['page'] == 'book')
-                {
+                } elseif (isset($_GET['page']) && $_GET['page'] == 'book') {
                     include('book/index.php'); //book
-                }
-                elseif(isset($_GET['page']) && $_GET['page'] == 'bank')
-                {
-                    include('bank/index.php'); //bank
-                }
-                elseif(isset($_GET['page']) && $_GET['page'] == 'category')
-                {
+                } elseif (isset($_GET['page']) && $_GET['page'] == 'bank') { //bank
+                    if(isset($_GET['function']) && $_GET['function'] == 'add')
+                    {
+                        include('bank/insert.php');
+                    }
+                    elseif(isset($_GET['function']) && $_GET['function'] == 'update')
+                    {
+                        include('bank/edit.php');
+                    }
+                    elseif(isset($_GET['function']) && $_GET['function'] == 'delete')
+                    {
+                        include('bank/delete.php');
+                    }
+                    else{
+                        include('bank/index.php'); 
+                    }
+                } elseif (isset($_GET['page']) && $_GET['page'] == 'category') {
                     include('category/index.php'); //category
-                }
-                elseif(isset($_GET['page']) && $_GET['page'] == 'orders_status')
-                {
+                } elseif (isset($_GET['page']) && $_GET['page'] == 'orders_status') {
                     include('order/index.php'); //order
                 }
                 ?>
--- admin_system/order/index.php
+++ admin_system/order/index.php
@@ -1,10 +1,44 @@
-<h1 class="app-page-title">Orders</h1>
+<?php
+require("connect_db.php");
+$sql = " SELECT * FROM payment"; //query DB
+$rs = mysqli_query($conn, $sql);
+?>
+<?php include('include/script.php') ?>
+<h1 class="app-page-title">Bank</h1>
 <hr class="mb-4">
 <div class="row g-4 setting-section">
-    <div class = "col-12 col-md-12">
+    <div class="col-12 col-md-12">
         <div class="app-card app-card-setting shadow-sm p-4">
             <div class="app-card-body">
+                <table class="table" id="tabelAll">
+                    <thead>
+                        <tr>
+                            <th scope="col">Bank No</th>
+                            <th scope="col">Bank Name</th>
+                            <th scope="col">Operation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($rs as $data) : ?>
+                            <tr>
+                                <td><?= $data['bank_no'] ?></td>
+                                <td><?= $data['bank_name'] ?></td>
+                                <td>
+                                    <a href="" class="btn btn-sm btn-warning">Edit</a>
+                                    <a href="" class="btn btn-sm btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#tabelAll').DataTable();
+    });
+</script>
+
+<?php mysqli_close($conn); ?>
