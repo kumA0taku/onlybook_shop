8bac043d591b23270ffa2d68659f1b8a8325c84c
SuKanyaPo <Sukanya.po@ku.th>
2022-04-07 17:43:14 +0700

    add file

--- admin_system/index.php
+++ admin_system/index.php
@@ -14,6 +14,7 @@
                 if(!isset($_GET['page']) && empty($_GET['page']))
                 {
                     include('stock/index.php'); //stock
+                    
                 }
                 elseif(isset($_GET['page']) && $_GET['page'] == 'book')
                 {
