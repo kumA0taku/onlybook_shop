65a2b668dd44448028d96b7c85c8ac417ac20d2f
patjira05 <patjira05@gmail.com>
2022-04-08 22:33:30 +0700

    etwgt

---
+++ admin_system/book/edit.php
@@ -0,0 +1,19 @@
+?php include('../connect_db.php') ?>
+<?php include('include/script.php') ?>
+<h1 class="app-page-title">Bank</h1>
+<hr class="mb-4">
+<div class="row g-4 setting-section">
+    <div class="col-12 col-md-12">
+        <div class="app-card app-card-setting shadow-sm p-4">
+            <div class="app-card-body">
+
+                <div class="app-content pt-3 p-md-3 p-lg-4">
+                    <div class="container-xl">
+                        <h1 class="app-page-title" style="font-size: 120%;">Edit Bank</h1>
+                        <hr class="mb-4">
+                        <div class="row g-4 settings-section">
+                            <div class="col-12 col-md-8">
+                                <div class="app-card app-card-settings shadow-sm p-4">
+                                    <div class="app-card-body">
+                                        <?php
+                                        
