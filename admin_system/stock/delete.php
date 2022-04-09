f17a11cb482016f3358c7721899bdaeaa1b520e6
patjira05 <patjira05@gmail.com>
2022-04-09 18:29:44 +0700

    yigiy

--- admin_system/stock/delete.php
+++ admin_system/stock/delete.php
@@ -1,22 +1,37 @@
 
 <?php
+ include('../connect_db.php');
+  include('include/script.php');
+  $request = $_REQUEST;
+  $id =$request['stock_id'];
+  $mysqli = new mysqli($conn);
+  if($mysqli->connect_errno){
+      echo "Failed to connect to MYSQL: ".$mysqli->connect_error;
+      exit();
+  }
+  $sql = "DELETE FROM stock WHERE id='".$id."'";
+  if($mysqli->query($sql)){
+      echo "stock has been successfully delete.";
+  }else{
+      echo "Error: ". $sql."<br>".$mysqli->error;
+  }
+  $mysqli->close();
+// if(isset($_GET['stock_id']) && !empty($_GET['stock_id'])){
+//     $id = $_GET["stock_id"];
+//     $sql="DELETE FROM bank WHERE id ='$id'";
+//    }   //print_r($_POST);
+//    if(mysqli_query($conn,$sql)){
+//     $alert = '<script type="text/javascript">';
+//     $alert .= 'alert("ลบข้อมูลสำเร็จ");';
+//     $alert .= 'windows.location.href = "?page=book";';
+//     $alert .= '</script>';
+//     echo $alert;
+//     exit();
 
-if(isset($_GET['book_id']) && !empty($_GET['book_id'])){
-    $id = $_GET["book_id"];
-    $sql="DELETE FROM bank WHERE book_id ='$id'";
-   }   //print_r($_POST);
-   if(mysqli_query($conn,$sql)){
-    $alert = '<script type="text/javascript">';
-    $alert .= 'alert("ลบข้อมูลสำเร็จ");';
-    $alert .= 'windows.location.href = "?page=book";';
-    $alert .= '</script>';
-    echo $alert;
-    exit();
+// }
+// else{
+//     echo "Error: " .$sql."<br>" . mysqli_error($conn);
+// }
 
-}
-else{
-    echo "Error: " .$sql."<br>" . mysqli_error($conn);
-}
-
-   mysqli_close($conn);
+//    mysqli_close($conn);
 ?>
